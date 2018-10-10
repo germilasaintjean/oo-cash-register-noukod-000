38b3b5a82c440a836eaa82e7cb8d93004eddb2fe
Saint-Jean Germila <germila@noukod.com>
2018-10-10 15:11:28 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,11 +1,14 @@
 class CashRegister
 
+attr_accessor :cash_register
 
-  def initialize cash_register(cash_register)
+  def initialize (cash_register = 0)
     @cash_register = cash_register
   end
 
-
+def cash_register_with_discount
+  self. cash_register += 1
+ende
 
 
 
