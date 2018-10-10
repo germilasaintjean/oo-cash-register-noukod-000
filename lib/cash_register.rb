8f72d324f3d216b13fa597856473a861fb11e307
Saint-Jean Germila <germila@noukod.com>
2018-10-10 15:24:30 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,13 +1,13 @@
 class CashRegister
 
-attr_accessor :total, :discount 
+attr_accessor :total, :discount
 
   def initialize (cash_register = 0)
     @total = cash_register
     @discount = cash_register
 end
 def add_item(title)
-   title.cash_register= ""
+   title.cash_register = ""
   price.cash_register = 0
   self.total =+1
 end
