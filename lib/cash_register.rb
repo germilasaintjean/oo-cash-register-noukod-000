fed3b6002f7c387d0ab4f655fda6ec18786d6d50
Saint-Jean Germila <germila@noukod.com>
2018-10-10 16:03:15 +0000

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,12 +1,31 @@
-class Person
+class CashRegister
 
-  attr_accessor :age
+  attr_accessor :items, :discount, :total, :last_transaction
 
-  def initialize(age = 0)
-    @age = age
+  def initialize(discount=0)
+    @total = 0
+    @discount = discount
+    @items = []
   end
 
-  def birthday
-    self.age += 1
+  def add_item(title, amount, quantity=1)
+    self.total += amount * quantity
+    quantity.times do
+      items << title
+    end
+    self.last_transaction = amount * quantity
+  end
+
+  def apply_discount
+    if discount != 0
+      self.total = (total * ((100.0 - discount.to_f)/100)).to_i
+      "After the discount, the total comes to $#{self.total}."
+    else
+      "There is no discount to apply."
+    end
+  end
+
+  def void_last_transaction
+    self.total = self.total - self.last_transaction
   end
 end
